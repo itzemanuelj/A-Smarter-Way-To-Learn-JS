d35d7eb39850b37398ad699bfeddcdbd0c0bd538
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-20 20:12:42 -0400

    commit to Alerts in js

---
+++ 1 Alerts/script.js
@@ -0,0 +1,31 @@
+/// Alerts
+
+/// Type the character that ends every statement.
+alert("hello world");
+
+/// Type the 2 characters that are missing in this statement.
+alert("youre learning javascript");
+
+/// A __________ ends in a semicolon.
+// statement
+
+alert("hi there sailor!");
+
+/// Code an alert that displays the message "Thanks for everything!"
+alert("thanks for everything");
+
+/// Code an alert that displays the message "Boo!"
+alert("boo!");
+
+/// The code displays a box with a message in it. Fill in the blank.
+/// ______("You won!");
+alert("you won!");
+
+/// Code an alert that displays the message "Hi"
+alert("hi!");
+
+///What are the 5 punctuation characters of an alert statement, in order?
+alert("statement");
+
+///Code an alert statement displaying any message you like.
+alert("you can have whatever you like!");
