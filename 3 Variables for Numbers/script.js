9a745c400515fe6c9fc1c3c8a338aa4099e5f3ef
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-22 12:17:29 -0400

    commit to a smarter way to learn js Variables for Numbers

---
+++ 3 Variables for Numbers/script.js
@@ -0,0 +1,45 @@
+/// Variables for Numbers
+
+///You can assign a string to a variable. You can also assign a
+let num = 1;
+
+///If a number is enclosed in quotes, it's a ______.
+let numAsStr = "one";
+
+///What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable? Answer with a 4-letter word. (It would be a five-letter word in British usage.)
+let math;
+
+/// What is the value of orderTotal?
+let merchTotal = 100;
+let shippingCharge = 10;
+let orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal); //110
+
+let niceBigNumber = 10000000;
+
+/// In a single statement, declare the variable numberAsString and assign the number 3 to it as a string
+let numberAsString;
+numberAsString = 3;
+
+///Assign to the variable caseQty, which has already been declared, the value 144.
+caseQty = 144;
+
+/// n a single statement declare a variable and assign a number to it. (You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to.)
+let numb = 10;
+
+/// In a single statement declare a variable and assign to it the sum of 2 other variables
+
+let a = 1;
+let b = 1;
+let c = a + b;
+
+/// Rewrite this statement so the variable can be used in a math operation.
+var number = "9";
+let number = 9;
+
+/// Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand
+let randomNumber = 1 + 1;
+
+/// one statement declare a variable. In a second statement assign it the sum of 2 numbers
+let randomNum;
+randomNum = 2;
