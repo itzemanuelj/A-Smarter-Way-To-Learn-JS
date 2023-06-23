8b076a430033e3e04402a366efc25a48814be4a9
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-23 02:11:56 -0400

    commit to a smarter way to learn js Variable Names Legal and Illegal

---
+++ 4 Variable Names Legal and Illegal/script.js
@@ -0,0 +1,40 @@
+/// Type the illegal character in this statement.
+// var product cost = 3.45;
+var productCost = 3.45;
+
+/// Rewrite this using camelCase.
+var Nameofband;
+let nameOfBrand;
+
+///Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.
+// let @#$^
+
+let averageHumanIntelligence;
+
+///In a single statement, declare the variable firstLegalChar and assign it a string. The string consists entirely of the first character on the keyboard in the top row that's legal in a variable name, as long as the character doesn't come first.
+let firstLegalChar = "$:_";
+
+///Declare a variable, in camelCase, that combines the two syllables boo and hoo
+let hooBooo;
+
+///In a single statement declare a legally-named variable and assign a number to it.
+let legalNumber = 1233;
+
+///In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+///Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+
+let combo = "!$_";
+
+/// The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+
+let boogieWoogie;
+boogieWoogie = "woogieBoogie";
+
+///Declare a variable that is a combination of your first and last names. Use camelCase.
+let emanuelJones;
