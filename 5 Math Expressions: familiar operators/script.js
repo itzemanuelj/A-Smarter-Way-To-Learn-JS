0fb5979d2f58aed69fc36077521f60892b665ea2
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-24 01:19:12 -0400

    commmit to a smarter way to learn js Math Expressions: familiar operators

---
+++ 5 Math Expressions: familiar operators/script.js
@@ -0,0 +1,38 @@
+///Math Expressions: familiar operators
+
+///Enter the operators, in this order, for addition, subtraction, multiplication, and division. Don't type spaces between them.
+// +_*/
+
+///What is the name of the arithmetic operator that gives you the remainder when one number is divided by another? (one word)
+
+let a = 10 % 5;
+console.log(a);
+
+///Type the modulus operator.
+let b = 100 % 33;
+console.log(b);
+
+///What is the value of num?
+var num = 20 % 6;
+console.log(num);
+//2
+
+let numberOfItems = totalCost + costPerItem;
+
+///In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000.
+let largeNum = 10000 * 2000;
+console.log(largeNum);
+
+///Assign to the variable num the result of dividing 9 by the value represented by the variable qty. The variable num hasn't been declared beforehand.
+let num = qty / 9;
+
+///Assign to a variable the result of adding a number to itself, for example 2 + 2. The variable hasn't been declared beforehand. Make up the variable name.
+let c = 2 + 2;
+
+///Code an alert that displays the result of any mathematical operation on 2 variables. Make up the variable names.
+let aa = b + c;
+
+///Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+
+let qw = ad / sd;
+console.log(qw);
