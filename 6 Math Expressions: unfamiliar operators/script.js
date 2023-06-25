0848b4dabadde165e2afa9d81e70518ba3b291ee
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-25 00:30:58 -0400

    commit to a smarter way to learn js Math Expressions: unfamiliar operators

---
+++ 6 Math Expressions: unfamiliar operators/script.js
@@ -0,0 +1,36 @@
+/// Math Expressions: unfamiliar operators
+
+///Code a short form of x = x + 1; Use either of the two legal expressions.
+x += 1;
+
+///If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression? Use either of the two legal expression
+x--;
+
+///var x = 50;
+var y = x++;
+/// What is the value of y?
+50;
+
+let newNum = (num += 1);
+
+///In a single statement, increment the variable num and assign its new value to a second variable, newNum, which hasn't been declared beforehand.
+num = newNum += 1;
+
+///In a single statement, decrement num and assign its original value to newNum, which hasn't been declared beforehand.
+newNum = num--;
+
+/// Using minimal code, decrement x. Use either of the two legal expressions.
+x--;
+
+/// What is the long version of x++?
+x = x + 1;
+
+/// In a single statement subtract 1 from a variable and assign the new value to another variable, which hasn't been declared beforehand. Both variables wind up with the same value.
+let a = b--;
+
+/// Increment a variable. Use either of the two legal expressions. In a second statement, display its new value in an alert.
+
+let c = 1;
+c += 1;
+alert(c);
+console.log(c);
